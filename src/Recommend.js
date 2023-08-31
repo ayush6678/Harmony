63acc1dd9341678ea8498f809a1291498b2d62de
Ayush <ayush.6678@gmail.com>
2023-09-01 02:00:26 +0530

    revamped entire UI

--- src/Recommend.js
+++ src/Recommend.js
@@ -5,7 +5,11 @@ import { useEffect, useState } from "react";
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 import PauseCircleFilledIcon from '@mui/icons-material/PauseCircleFilled';
 import { IconButton } from "@mui/material";
-import Queue from "./Queue.js";
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import PersonIcon from '@mui/icons-material/Person';
+import './Queue.css';
+import Upload from "./Upload.js";
 
 function Recommend() {
     // const axios=require('axios');
@@ -38,6 +42,7 @@ function Recommend() {
     }
 
     const [songLink, setSongLink] = useState("");
+    const [last, setLast] = useState("");
     const [isPlaying, setIsPlaying] = useState(false);
     const [current, setCurrent] = useState(["Song", "Artist", "https://t4.ftcdn.net/jpg/03/93/23/51/360_F_393235111_ygEWm52rXjI72T7pyJUOcsDRvf8rY3ON.jpg"]);
 
@@ -46,9 +51,14 @@ function Recommend() {
 
         if (isPlaying) {
             song.play();
-        } else {
+        }
+        else if (last !== songLink && last !== "") (
+            setIsPlaying(true)
+        )
+        else {
             song.pause();
         }
+
         return () => {
             song.pause();
         };
@@ -62,8 +72,17 @@ function Recommend() {
 
             <div className="search">
                 <input type="text" placeholder="Search here for songs" value={searchQuery} onChange={searchResults}></input>
-
-                <h1>Recommendation for you</h1>
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginLeft: '1.7%' }}>
+                    <h1>Recommendation for you</h1>
+                    <h3 style={{ color: '#DE631F', fontFamily: 'arial', fontWeight: '100', fontSize: '15px' }}>See all</h3>
+                </div>
+                <div className="choice" style={{ display: 'flex', justifyContent: 'space-between', marginLeft: '1.7%', paddingLeft: '10px', paddingRight: '200px' }}>
+                    <button>Popular</button>
+                    <button>Songs</button>
+                    <button>Artists</button>
+                    <button>Album</button>
+                    <button>Genre</button>
+                </div>
             </div>
 
 
@@ -73,6 +92,9 @@ function Recommend() {
 
                     return (<div style={{ display: "flex", flexDirection: "column" }} onClick={() => {
                         setIsPlaying(!isPlaying);
+
+                        setLast(songLink);
+
                         setSongLink(song.accessToken);
                         setCurrent([song.name, song.auth, song.image])
                     }}>
@@ -92,39 +114,119 @@ function Recommend() {
 
             <div className="Player">
 
-                <div style={{ display: "flex" }}>
-                    <img src={current[2]} width={50} alt="songArt" />
-                    <div className="songListing">
-                        {current[0]}
-                        <br />
-                        {current[1]}
+                <div className="topchart">
+                    <h1>Top chart</h1>
+
+                    <div style={{ height: '150px', overflowY: 'scroll' }}>
+
+                        {songs.map((song) => {
+                            return (
+                                <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+                                    <div style={{ borderRadius: '50%', overflow: 'hidden' }}>
+                                        <img src={song.image} width='25px' height='25px' alt="songArt" />
+                                    </div>
+                                    <div style={{ fontSize: "smaller", marginLeft: '15px' }}>
+                                        {song.name}<br />
+                                        <div style={{ fontSize: '10px', color: '#AAA6A6' }}>{song.auth}</div>
+                                    </div>
+                                    <div style={{ marginLeft: "auto" }}>
+                                        <PlayCircleIcon sx={{ color: '#B65959', background: 'white', borderRadius: '50%' }} />
+                                    </div>
+
+                                </div>
+                            );
+                        })}
                     </div>
                 </div>
 
-                <div>
-                    <IconButton onClick={() => {
-                        if (current.name !== "Song") {
-                            setIsPlaying(!isPlaying)
-                        }
-                    }}
-                    >
-                        {!isPlaying ? <PlayCircleIcon fontSize="large" sx={{ color: "white" }} /> :
-                            <PauseCircleFilledIcon fontSize="large" sx={{ color: "white" }} />}
-                    </IconButton>
+                <div className="liked">
+                    <h1>Liked Songs</h1>
+                    <div style={{ display: 'flex', padding: '5px', overflow: 'scroll', maxWidth: '50vw' }}>
+                        {songs.map((song) => {
+                            return (
+                                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '5px' }}>
+                                    <div style={{ padding: '10px' }}>
+                                        <div style={{ borderRadius: '50%', width: '60px', height: '60px', overflow: 'hidden' }}>
+                                            <img src={song.image} width='60px' height='60px' alt="songArt" />
+                                        </div>
+                                    </div>
+                                    <div style={{ fontSize: "smaller" }}>
+                                        {song.name}
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
 
-                <div>
-                    Etc<br />
-                    icons here
-                </div>
+
             </div>
 
             <div className="queue">
-                <Queue songs={songs}/>
+                <div className="Q">
+
+                    <div className="topp" style={{ display: "flex" }}>
+                        <IconButton sx={{ color: "white", borderRadius: "50%" }}>
+                            <ChatIcon />
+                        </IconButton>
+                        <IconButton sx={{ color: "white", borderRadius: "50%" }}>
+                            <NotificationsIcon />
+                        </IconButton>
+                        <IconButton sx={{ color: "white", borderRadius: "50%" }}>
+                            <PersonIcon />
+                        </IconButton>
+                        <div className="logo">
+                            <h1>Harmony</h1>
+                        </div>
+                    </div>
+
+                    <div className="upl">
+                        <Upload />
+                    </div>
+
+                    <div className="Lib">
+                        <h2>Next on Chillout Playlist</h2>
+                        <div className="Library">
+                            {songs.map((song) => {
+                                return (
+                                    <div className="miniList">
+                                        <img src={song.image} width={"20%"} height={"20%"} alt="songArt" />
+                                        <div style={{ fontSize: "smaller" }}>
+                                            {song.name}<br />
+                                            {song.auth}
+                                        </div>
+
+                                    </div>
+                                );
+                            })}
+                        </div>
+
+
+                        <div className="nowPlaying">
+                            <div className="playimg" style={{ maxHeight: '200px', marginBottom: '20px' }}>
+                                <img src={current[2]} width={"100%"} alt="songArt" />
+                            </div>
+
+                            <div className="textOverlay" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                <div>{current[0]}</div>
+                                <br />
+                                <div style={{ marginTop: '-15px' }}>{current[1]}</div>
+                                <div>
+                                    <IconButton onClick={() => {
+                                        if (current.name !== "Song") {
+                                            setIsPlaying(!isPlaying)
+                                        }
+                                    }}
+                                    >
+                                        {!isPlaying ? <PlayCircleIcon fontSize="large" sx={{ color: "white" }} /> :
+                                            <PauseCircleFilledIcon fontSize="large" sx={{ color: "white" }} />}
+                                    </IconButton>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-
-
         </div >
     )
 }
--- src/Upload.js
+++ src/Upload.js
@@ -3,18 +3,9 @@ import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/
 import { useState } from 'react';
 import "./Upload.css"
 import axios from "axios";
+import { createClient } from '@supabase/supabase-js'
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAINyNbt7xoNUzjmsUR-WxxSLnAPmN8PNU",
-    authDomain: "harmony-2ed98.firebaseapp.com",
-    projectId: "harmony-2ed98",
-    storageBucket: "harmony-2ed98.appspot.com",
-    messagingSenderId: "1039945448650",
-    appId: "1:1039945448650:web:05f8eda461d599db058833"
-};
-
-const app = initializeApp(firebaseConfig);
-const storage = getStorage(app);
+const supabase = createClient('https://qhfeezkilpoxrueywxsz.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoZmVlemtpbHBveHJ1ZXl3eHN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMyNjY1OTYsImV4cCI6MjAwODg0MjU5Nn0.O8GRQD0S3eNL9rFyD_-7omHzs_p4X1awTEI6Crtoq80')
 
 function Upload() {
 
@@ -30,37 +21,48 @@ function Upload() {
         setFile(event.target.files[0]);
     }
 
-    const handleUpload = () => {
-        if (!file) {
-            alert("Please upload an image first!");
+
+
+        async function handleUpload(file) {
+            if (!file) {
+                alert("Please upload an image first!");
+            }
+
+            const { data, error } = await supabase.storage.from('harmonySongStorage').upload('/home/ayyushh/Downloads/believer.mp3', file)
+            if (error) {
+                console.log("nope"+error+data);
+            } else {
+                // Handle success
+                console.log("YUpp");
+
+            }
         }
 
-        const storageRef = ref(storage, `/${file.name}`);
-        const uploadTask = uploadBytesResumable(storageRef, file);
+        //     const storageRef = ref(storage, `/${file.name}`);
+        //     const uploadTask = uploadBytesResumable(storageRef, file);
 
-        uploadTask.on(
-            "state_changed",
-            (snapshot) => {
-                const percent = Math.round(
-                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-                );
-                setPercent(percent);
-            },
-            (err) => console.log(err),
-            () => {
-                getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-                    console.log(url);
+        //     uploadTask.on(
+        //         "state_changed",
+        //         (snapshot) => {
+        //             const percent = Math.round(
+        //                 (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        //             );
+        //             setPercent(percent);
+        //         },
+        //         (err) => console.log(err),
+        //         () => {
+        //             getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+        //                 console.log(url);
 
-                    axios.post("https://harmony-backend-1rjg.onrender.com/songData", {
-                        name: name,
-                        auth: auth,
-                        image: image,
-                        accessToken: url
-                    })
-                });
-            }
-        );
-    };
+        //                 axios.post("https://harmony-backend-1rjg.onrender.com/songData", {
+        //                     name: name,
+        //                     auth: auth,
+        //                     image: image,
+        //                     accessToken: url
+        //                 })
+        //             });
+        //         }
+        //     );
 
     return (
         <div className="Upload" style={{ padding: disp ? "20px" : "0" }}>
@@ -70,8 +72,8 @@ function Upload() {
 
             <div className="expanded" style={{ display: disp ? "" : "none" }}>
                 <h1>Upload A Song</h1>
-                <input type="text" onChange={(e) => { setName(e.target.value) }} placeholder="Name of the song"  /><br />
-                <input type="text" onChange={(e) => { setAuth(e.target.value) }} placeholder="Artist"  /><br />
+                <input type="text" onChange={(e) => { setName(e.target.value) }} placeholder="Name of the song" /><br />
+                <input type="text" onChange={(e) => { setAuth(e.target.value) }} placeholder="Artist" /><br />
                 <input type="text" onChange={(e) => { setImage(e.target.value) }} placeholder="Link to Song Art" /><br />
 
                 <input type="file" onChange={handleChange} style={{
